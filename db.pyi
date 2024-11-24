6ab76221bf3c0c9f4d6fbf47f07f59ede04a2aa7
Arthur Havlicek <ahavlicek@letsignit.com>
2024-11-24 19:54:29 +0100

    remove unecessary stub

--- db.pyi
+++
@@ -1,9 +0,0 @@
-import psycopg
-from .domain import Player
-
-class Session:
-    @classmethod
-    async def connect(cls) -> "Session": ...
-    conn: psycopg.AsyncConnection
-    def __init__(self, conn: psycopg.AsyncConnection) -> None: ...
-    async def create_player(self, name: str) -> Player: ...
